75373342ea0256f47b2d70d543c0697ea3892387
AnnaLapushkina <anchencia@bk.ru>
2015-11-24 14:37:12 +0300

    Update ValueOutOfRangeTest.java

--- NumberGuess/test/numberGuess/ValueOutOfRangeTest.java
+++ NumberGuess/test/numberGuess/ValueOutOfRangeTest.java
@@ -2,26 +2,23 @@ package numberGuess;
 
 import static org.junit.Assert.*;
 
-import java.util.Random;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ValueOutOfRangeTest {
-	private static final int LEFT_BORDER = 1;
-	private static final int RIGHT_BORDER = 100;
+
 	private static final String OUT_OF_RANGE_LEFT = "-3";
-	private static final String OUT_OF_RANGE_RIGHT = "101";
+	private static final String OUT_OF_RANGE_RIGHT = "105";
+	private static final String WRONG_VALUE = "%#$%";
 	private static final String MESSAGE = "a number next time";
 
 	private NumberGuessBean numberGuessBean;
-	private Random random;
 
 	@Before
 	public void createObject() {
 		this.numberGuessBean = new NumberGuessBean();
-		this.random = new Random();
+
 	}
 
 	@After
@@ -32,26 +29,16 @@ public class ValueOutOfRangeTest {
 	@Test
 	public void testOutOfRange() {
 
-		int range = this.random.nextInt();
-		int value;
-
-		if (range >= LEFT_BORDER && range <= RIGHT_BORDER) {
-			range -= 200;
-		}
-		value = this.numberGuessBean.getNumGuesses();
 		this.numberGuessBean.setGuess(OUT_OF_RANGE_LEFT);
 		assertEquals(MESSAGE, this.numberGuessBean.getHint());
-		assertEquals(value + 1, this.numberGuessBean.getNumGuesses());
 
-		value = this.numberGuessBean.getNumGuesses();
 		this.numberGuessBean.setGuess(OUT_OF_RANGE_RIGHT);
 		assertEquals(MESSAGE, this.numberGuessBean.getHint());
-		assertEquals(value + 1, this.numberGuessBean.getNumGuesses());
 
-		value = this.numberGuessBean.getNumGuesses();
-		this.numberGuessBean.setGuess(String.valueOf(range));
+		this.numberGuessBean.setGuess(WRONG_VALUE);
 		assertEquals(MESSAGE, this.numberGuessBean.getHint());
-		assertEquals(value + 1, this.numberGuessBean.getNumGuesses());
+
 	}
 
 }
+
